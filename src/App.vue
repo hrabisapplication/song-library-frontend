6bd2180fa0410868318930158f60e5c2152793cd
DESKTOP-5M7088H\hrabi <hrabisapplication@seznam.cz>
2023-03-24 03:16:21 +0100

    nový projekt

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title>Song Library</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn to="/" exact>HomePage</v-btn>
+      <v-btn to="/bands">Bands</v-btn>
+      <v-btn to="/songs">Songs</v-btn>
+    </v-app-bar>
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "App",
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/* Add your own global CSS styles here */
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import './assets/styles.css'
+import 'vuetify/dist/vuetify.css'
+import { createVuetify } from 'vuetify'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+const vuetify = createVuetify()
+
+app.use(router)
+app.use(store)
+app.use(vuetify)
+
+app.mount('#app')
